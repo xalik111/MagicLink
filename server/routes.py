b8c4af7ed52d3a09f3b52b1bc1b3d5f4c3b7bc7e
XALIK\xalik <xalik@meta.ua>
2021-02-08 23:32:34 +0200

    6 commit

--- server/routes.py
+++ server/routes.py
@@ -5,7 +5,7 @@ from server import app, socketio
 # A welcome message to test our server
 @app.route('/')
 def index():
-    return "<h1>Welcome to our server !!!!</h1>"
+    return "<h1>Welcome to our server!!!!</h1>"
 
 
 @app.route('/create', methods=['GET', 'POST'])
