db96987a6e112df57c47a63fac2543a867990465
XALIK\xalik <xalik@meta.ua>
2021-02-10 01:32:10 +0200

    6 commit

--- server/routes.py
+++ server/routes.py
@@ -46,7 +46,7 @@ def magic_link(link):
         user = Users.select().where(Users.magiclink == link).get()
         login_user(user)
         user.update(url_counter=user.url_counter+1)
-        user.execute()
+        #user.execute()
         return redirect(url_for('afterlogin'))
     except Exception as ex:
         return str(ex)
