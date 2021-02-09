b05c022f83a2c2888bfabb3bea6085e55f28b9dc
XALIK\xalik <xalik@meta.ua>
2021-02-09 22:09:31 +0200

    6 commit

---
+++ server/models.py
@@ -0,0 +1,27 @@
+from flask_login import UserMixin
+from peewee import SqliteDatabase, IntegerField, CharField, TextField, \
+    BigIntegerField, Model, AutoField, DateTimeField, ForeignKeyField
+
+from server import manager
+
+# SQLite database using WAL journal mode and 64MB cache.
+sqlite_db = SqliteDatabase('app.db', pragmas={
+    'journal_mode': 'wal',
+    'cache_size': -1024 * 64})
+
+class Users(Model, UserMixin):
+    id = IntegerField()
+    login = TextField(unique=True)
+    password = TextField()
+    magiclink = TextField()
+    url_counter = IntegerField()
+
+    class Meta:
+        database = sqlite_db
+
+@manager.user_loader
+def load_user(user_id):
+    try:
+        return Users.get(user_id)
+    except Exception as identifier:
+        print(identifier)
