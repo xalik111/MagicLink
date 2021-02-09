ebc38dcf0066a50db35f4bc848c5540006b4a7e0
XALIK\xalik <xalik@meta.ua>
2021-02-09 23:39:25 +0200

    6 commit

--- server/routes.py
+++ server/routes.py
@@ -28,7 +28,8 @@ def index(email):
         hash_pwd = generate_password_hash('Qwerty123')
         magiclink = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
         Users.create(login=login, password=hash_pwd, magiclink=magiclink, url_counter=0)
-        return 'User %s created %s' % escape(email), magiclink
+        first = "User %s created." % escape
+        return 'User %s created %s' % (escape(email), magiclink)
     
 
 @app.route('/afterlogin', methods=['GET', 'POST'])
