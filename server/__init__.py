98606c55451307767d14a67d7829362e6dbc5172
XALIK\xalik <xalik@meta.ua>
2021-02-15 19:56:51 +0200

    6 commit

--- server/__init__.py
+++ server/__init__.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template_string, render_template_string, request, jsonify
 from flask_login import LoginManager
+from flask_mail import Mail, Message
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
+mail= Mail(app)
 app.secret_key = 'somesecretsalt'
 manager = LoginManager(app)
 async_mode = None
 socketio = SocketIO(app, async_mode=async_mode)
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'xalikxalik44@gmail.com'
+app.config['MAIL_PASSWORD'] = 'Awesomest!(98'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+mail = Mail(app)
+
 
 from server import routes, models
