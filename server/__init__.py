f810cf0d5c8c9e698b4df572c10b0c0063f0eceb
XALIK\xalik <xalik@meta.ua>
2021-02-15 21:15:12 +0200

    6 commit

--- server/__init__.py
+++ server/__init__.py
@@ -14,7 +14,7 @@ app.config.update(dict(
     MAIL_SERVER = 'smtp.gmail.com',
     MAIL_PORT = 587,
     MAIL_USE_TLS = True,
-    MAIL_USE_SSL = False,
+    MAIL_USE_SSL = True,
     MAIL_USERNAME = 'xalikxalik44@gmail.com',
     MAIL_PASSWORD = 'Awesomest!(98',
 ))
