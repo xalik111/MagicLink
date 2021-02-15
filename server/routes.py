3ea8a21bb29332564ec5d70e634a4f4359652b55
XALIK\xalik <xalik@meta.ua>
2021-02-15 22:26:45 +0200

    6 commit

--- server/routes.py
+++ server/routes.py
@@ -19,6 +19,18 @@ from .models import Users
 def main():
     return "<h1>Welcome to our server!!!!</h1>"
 
+@app.route("/mail")
+def send_mail():
+    sg = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
+    from_email = Email("magiclinktest@heroku.com")
+    to_email = To("xalikxalik44@gmail.com")
+    subject = "Sending with SendGrid is Fun"
+    content = Content("text/plain", "and easy to do anywhere, even with Python")
+    mail = Mail(from_email, to_email, subject, content)
+    response = sg.client.mail.send.post(request_body=mail.get())
+    return str(response.status_code)
+
+
 @app.route('/create', methods=['GET', 'POST'])
 def emailform():
     return render_template("create.html")
